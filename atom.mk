3718d53d157ef0abb19349395b062a79c29883a1
Aurélien Barre <aurelien.barre@parrot.com>
2017-11-03 00:17:06 +0100

    [BUILD] add exports to atom.mk

--- atom.mk
+++ atom.mk
@@ -11,6 +11,16 @@ LOCAL_SRC_FILES := \
 	ilcore.c
 LOCAL_INSTALL_HEADERS := \
 	ilclient.h:usr/include/
+LOCAL_EXPORT_CFLAGS := -DSTANDALONE -D__STDC_CONSTANT_MACROS \
+	-D__STDC_LIMIT_MACROS -DTARGET_POSIX -D_LINUX -fPIC -DPIC \
+	-D_REENTRANT -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 \
+	-U_FORTIFY_SOURCE -Wall -g -DHAVE_LIBOPENMAX=2 \
+	-DOMX -DOMX_SKIP64BIT -ftree-vectorize -pipe \
+	-DUSE_EXTERNAL_OMX -DHAVE_LIBBCM_HOST \
+	-DUSE_EXTERNAL_LIBBCM_HOST -DUSE_VCHIQ_ARM -Wno-psabi
+LOCAL_EXPORT_C_INCLUDES := $(SDKSTAGE)/opt/vc/include
+LOCAL_EXPORT_LDLIBS += -L$(SDKSTAGE)/opt/vc/lib -lbrcmGLESv2 -lbrcmEGL \
+	-lopenmaxil -lbcm_host -lvcos -lvchiq_arm -lpthread -lrt -lm
 
 LOCAL_CFLAGS += -DSTANDALONE -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS -DTARGET_POSIX -D_LINUX -fPIC -DPIC -D_REENTRANT -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -U_FORTIFY_SOURCE -Wall -g -DHAVE_LIBOPENMAX=2 -DOMX -DOMX_SKIP64BIT -ftree-vectorize -pipe -DUSE_EXTERNAL_OMX -DHAVE_LIBBCM_HOST -DUSE_EXTERNAL_LIBBCM_HOST -DUSE_VCHIQ_ARM -Wno-psabi
 LOCAL_CFLAGS += -I$(SDKSTAGE)/opt/vc/include/ -I$(SDKSTAGE)/opt/vc/include/interface/vcos/pthreads -I$(SDKSTAGE)/opt/vc/include/interface/vmcs_host/linux -I./ -I$(SDKSTAGE)/opt/vc/src/hello_pi/libs/ilclient -I$(SDKSTAGE)/opt/vc/src/hello_pi/libs/vgfont
